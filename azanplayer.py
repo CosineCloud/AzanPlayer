0250d75b9239fa2cb740218e06cee9c08268196d
Talha Haroon <71771896+CosineCloud@users.noreply.github.com>
2022-11-10 16:21:06 +0800

    azanplayer v1.0
    IMPORTANT NOTE : MP3 files need to add in same folder where your saving this script and use same file name as mentioned in script.

--- azanplayer.py
+++ azanplayer.py
@@ -1,6 +1,5 @@
 import schedule
 import time
-#from tkinter import *
 import requests
 import json
 import alsaaudio
@@ -13,12 +12,12 @@ try:
         from datetime import datetime
         now = datetime.now()
         dt_string = now.strftime("%Y-%m-%d")
-        # required parameters
+        # required parameters // local Lat Long
         lat = 3.078622497747135
         long = 101.5206969400765
         calc_method = 'karachi'
         date = str(dt_string)
-        # optional parameters
+        # optional parameters // define school of thought
         school = "shafi"
         midnightMode = "jafari"
         latitudeAdjustmentMethod = "middle of the night"
@@ -214,21 +213,6 @@ def Azan_isha():
     time.sleep(35)
     p.stop()
 
-# Functions setup
-def sudo_placement():
-    print("Get ready for Sudo Placement at Geeksforgeeks")
- 
-def good_luck():
-    print("Good Luck for Test")
- 
-def work():
-    print("Study and work hard")
- 
-def bedtime():
-    print("It is bed time go rest")
-     
-def geeks():
-    print("Shaurya says Geeksforgeeks")
  
 def blutoothcon():
      output_stream = os.popen('pulseaudio --start')
@@ -244,42 +228,18 @@ def blutoothcon():
      print (output_stream.read())
      time.sleep(2)
 # Task scheduling
-#After every 10mins blutoothcon() is called.
+#After every 10mins blutoothcon() is called. // You can skip this if not required
 schedule.every(10).minutes.do(blutoothcon)
- 
-# After every hour geeks() is called.
-#schedule.every().hour.do(geeks)
- 
-# Every day at 12am or 00:00 time bedtime() is called.
-#Prayertimings()
-#Azan_zohar()
+#Set volumet to Max // Can set volume to each of the azan indivisually
 vol(100)
-#blutoothcon()
-#schedule.every().day.at("20:17").do(Azan_maghrib)
 schedule.every().day.at(str(fajr())).do(Azan_fajir)
 schedule.every().day.at(str(Dhuhr())).do(Azan_zohar)
 schedule.every().day.at(str(Asr())).do(Azan_asar)
 schedule.every().day.at(str(Maghrib())).do(Azan_maghrib)
 schedule.every().day.at(str(Isha())).do(Azan_isha)
-#schedule.every().day.at("23:18").do(Azan_maghrib)
 
-
-
- 
-# After every 5 to 10mins in between run work()
-#schedule.every(5).to(10).minutes.do(work)
- 
-# Every monday good_luck() is called
-#schedule.every().monday.do(good_luck)
- 
-# Every tuesday at 18:00 sudo_placement() is called
-#schedule.every().tuesday.at("18:00").do(sudo_placement)
- 
-# Loop so that the scheduling task
-# keeps on running all time.
 while True:
  
-    # Checks whether a scheduled task
     # is pending to run or not
     schedule.run_pending()
     time.sleep(1)
